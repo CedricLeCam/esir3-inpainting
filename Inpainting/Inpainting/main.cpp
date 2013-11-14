af9256f91b725bb9663216e36a83e816a151e2db
Cedric ESIR <cedric.esir.doc@gmail.com>
2013-11-14 16:20:28 +0100

    premiers essais

--- Inpainting/Inpainting/main.cpp
+++ Inpainting/Inpainting/main.cpp
@@ -1,3 +1,33 @@
+#include <iostream>
+#include <opencv2\core\core.hpp>
+#include <opencv2\highgui\highgui.hpp>
+
+using namespace std;
+using namespace cv;
+
 int main(int argc, char ** argv) {
+
+	cout << "start" << endl;
+
+	//ouvrir une image
+	Mat img = imread("../data/lena.bmp", CV_LOAD_IMAGE_COLOR);
+
+	int taille = img.size().area();
+
+	if (!img.data) {
+		cout << "echec" << endl;
+	}
+
+	cout << taille << endl;
+
+	//cout << img << endl;
+	//afficher l'image
+	namedWindow("lena", CV_WINDOW_AUTOSIZE);
+	imshow("lena", img);
+
+	waitKey(0);
+
+	cout << "succes2" << endl;
+
 	return 0;
 }
