f4dbc77f60733974cf6543cdda1e5710374b6084
Cedric ESIR <cedric.esir.doc@gmail.com>
2013-12-02 17:30:35 +0100

    mise en place template matching

--- Inpainting/Inpainting/main.cpp
+++ Inpainting/Inpainting/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <vector>
 #include <opencv2\core\core.hpp>
 #include <opencv2\highgui\highgui.hpp>
 #include <opencv2\imgproc\imgproc.hpp>
@@ -25,6 +26,10 @@ int main(int argc, char ** argv) {
 		return 0;
 	}
 
+	//proprietes pratiques
+	int height = img.size().height;
+	int width = img.size().width;
+
 	//afficher l'image
 	namedWindow("lena", CV_WINDOW_AUTOSIZE);
 	imshow("lena", img);
@@ -38,8 +43,13 @@ int main(int argc, char ** argv) {
 	Mat roi(mask, Rect(100,100,50,50));
 
 	//appliquer roi à l'image, masque nul
-	roi = Scalar(0,0,0);
+	roi = Scalar(0);
 
+	//afficher le masque
+	namedWindow("masque", CV_WINDOW_AUTOSIZE);
+	imshow("masque", mask);
+
+	waitKey(0);
 
 	/*--------------------------------------
 	Priorités
@@ -72,13 +82,22 @@ int main(int argc, char ** argv) {
 	waitKey(0);
 
 
-	/*
+	
 	
 	//ligne de front contenue dans le laplacien
 
 	//binariser le laplacien
+	threshold(laplace, laplace, 1.0, 255.0, THRESH_BINARY);
+
+	//inspection de qqs pixels
+	for (int i = 98 ; i<103 ; i++) {
+		for ( int j=98 ; j < 103 ; j++) {
+			cout << "pixel : " << i << " , " << j << " " << laplace.at<float>(i,j) << endl;
+		}
+	}
 
 
+	
 	//calcul des priorités
 
 	//paramètres du patch
@@ -87,25 +106,44 @@ int main(int argc, char ** argv) {
 	float invCard = 1.0/(t_patch*t_patch);
 
 
+	//retenir la dernière plus haute priorité
+	int y_prior = 0;
+	int x_prior = 0;
+	float prior = 0.0;
+
+
 	for (int y=0 ; y < laplace.size().height ; y++) {
 		for (int x=0 ; x < laplace.size().width ; x++) {
 
 			//si ligne de front
-			if (laplace.at<float>(y,x) != 0) {
+			if (laplace.at<float>(y,x) == 255.0) {
 				//compter connus et inconnus, appliquer patch
 				
 				int connus = 0;
 				for (int i=0 ; i<t_patch ; i++) {
 					for (int j=0 ; j<t_patch ; j++) {
-						if (mask.at<uchar>(y-half+i,x-half+j) =! 0) {
+
+
+						if (mask.at<uchar>(y-half+i,x-half+j) > 0) {
 							//connu
 							connus++;
 						}
+						
 					}
 				}
+				
 				//fin de compte
 				//mettre à jour priorité
-				laplace.at<float>(y,x) = connus*invCard;
+				float loc_prior = ((float)connus) * invCard;
+				laplace.at<float>(y,x) = loc_prior;
+
+
+				//retenir la plus haute priorité
+				if (loc_prior >= prior) {
+					prior = loc_prior;
+					y_prior = y;
+					x_prior = x;
+				}
 
 			}
 
@@ -113,14 +151,47 @@ int main(int argc, char ** argv) {
 	}
 	//fin du calcul des priorités initiales
 
-	cout << laplace.at<float>(101,150) << endl;
+	//inspection de qqs pixels
+	for (int i = 98 ; i<103 ; i++) {
+		for ( int j=98 ; j < 103 ; j++) {
+			cout << "pixel : " << i << " , " << j << " " << laplace.at<float>(i,j) << endl;
+		}
+	}
 
 	namedWindow("laplace", CV_WINDOW_AUTOSIZE);
 	imshow("laplace", laplace);
 	
 	waitKey(0);
 	
-	*/
+	//remplissage sur la plus haute priorite
+
+	//template matching
+
+	//parametres
+	float t_patch = 9.0;
+	int half = t_patch/2;
+
+	//récupération des pixels connus du patch autour de la priorité
+	vector<Point> connus;
+
+	for (int j=y_prior-half ; j < y_prior+half+1 ; j++) {
+		for (int i = x_prior-half ; i < x_prior+half+1 ; i++) {
+			
+			if(mask.at<uchar>(j,i) > 0) {
+				//connu
+				Point p(j,i);
+				connus.push_back(p);
+			}
+		}
+	}
+
+
+	//scan de l'image
+	for (int y = half ; y < height-half ; y++) {
+		for (int x = half ; x < width-half ; x++) {
+
+			//matching sur les pixels connus
+
 
 	/*
 	namedWindow("lena_trou", CV_WINDOW_AUTOSIZE);
